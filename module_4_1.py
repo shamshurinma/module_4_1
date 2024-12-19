2e1ae96566186138c208a977d05a88782a5e9b52
Mikhail Shamshurin <shamshurinma@gmail.com>
2024-12-19 22:34:18 +0400

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+
+result_1 = fm(69, 3)
+result_2 = fm(3, 0)
+result_3 = tm(49, 7)
+result_4 = tm(15, 0)
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
